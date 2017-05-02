fb5b0d934b53f5c57704a8580bc0b6eb812aec3d
Ken Sodemann <ken.sodemann@gmail.com>
2017-05-02 18:23:53 -0500

    chore(build): script the updating of release information

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var git = require('gulp-git');
 var jshint = require('gulp-jshint');
 
+// Development Tasks
 gulp.task('deleteLintLog', function(done) {
   var del = require('del');
   del(['jshint-output.log']).then(function() {
@@ -47,8 +50,59 @@ gulp.task('test', function() {
     .pipe(mocha({ reporter: 'nyan' }));
 });
 
-gulp.task('default', ['lint', 'style', 'test']);
+// Release Tasks
+gulp.task('checkReleaseParams', function() {
+  if (!gutil.env.type) {
+    console.error('You must specify a type of release [major, minor, patch]');
+    process.exit(1);
+  }
 
+  if ((gutil.env.type !== 'major' && gutil.env.type !== 'minor' && gutil.env.type !== 'patch')) {
+    console.error('release type must be "major", "minor", or "patch"');
+    process.exit(1);
+  }
+});
+
+var nextVersion;
+gulp.task('generateNextVersion', ['checkReleaseParams'], function() {
+  var fs = require('fs');
+  var semver = require('semver');
+  var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+  nextVersion = semver.inc(pkg.version, gutil.env.type);
+  console.log(nextVersion);
+});
+
+gulp.task('bumpVersion', ['generateNextVersion'], function() {
+  bump = require('gulp-bump');
+  return gulp.src('./package.json')
+    .pipe(bump({ version: nextVersion }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('generateChangelog', ['bumpVersion'], function() {
+  var conventionalChangelog = require('gulp-conventional-changelog')
+  return gulp.src('CHANGELOG.md')
+    .pipe(conventionalChangelog({
+      preset: 'angular'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('commitRelease', ['generateChangelog'], function() {
+  return gulp.src(['./CHANGELOG.md', './package.json'])
+    .pipe(git.add())
+    .pipe(git.commit('chore: release version ' + nextVersion));
+});
+
+gulp.task('tagRelease', ['commitRelease'], function() {
+  git.tag(nextVersion, '', function (err) {
+    if (err) throw err;
+  });
+});
+
+// End user tasks
+gulp.task('default', ['lint', 'style', 'test']);
 gulp.task('dev', ['default'], function() {
   return gulp.watch(__dirname + '/**/*.js', ['default']);
 });
+gulp.task('release', ['tagRelease']);
